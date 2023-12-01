3cbc69a0052ea1e6dd054d563ecd65b4ab6ed741
Mason Stooksbury <masonstooksbury@gmail.com>
2023-12-01 15:19:42 -0600

    Modified code so that there are fewer file reads. Fixed the short-write sending for page serving. And commented everything

--- PicoWCode/main.py
+++ PicoWCode/main.py
@@ -1,206 +1,47 @@
-from phew import access_point, dns, server
-from phew.template import renderPage, renderOther
-from phew.server import redirect
-import network, time, socket
+import network, socket
 from time import sleep
-import machine
 import json
-import os
-import utime
 
 AP_NAME = "Hex Clock"
-AP_PASSWORD = "passwordfuck"
+AP_PASSWORD = "password123"
 AP_DOMAIN = "hexclock.com"
 PAGES_PATH = "pages"
-# WIFI_MAX_ATTEMPTS = 3
 CONFIG_FILE_PATH = 'config.json'
 
 
 
+# Returns the desired values from the config file as a dictionary
+def getValuesFromConfig(*args):
+    with open(CONFIG_FILE_PATH, 'r') as file:
+        data = json.load(file)
 
-# def machine_reset():
-#     utime.sleep(1)
-#     print("Resetting...")
-#     machine.reset()
+    # Create an object composed of the desired keys from the config file
+    results = {}    
+    for key in args:
+        results[key] = data.get(key)
 
-# def getValueFromConfig(value):
-#     with open(CONFIG_FILE_PATH, "r") as f:
-#         data = json.load(f)
-#     return data[value]
-
-
-
+    return results
 
 
 
-
-# # # microsoft windows redirects
-# # @server.route("/ncsi.txt", methods=["GET"])
-# # def hotspot(request):
-# #     print(request)
-# #     print("ncsi.txt")
-# #     return "", 200
-
-
-# # @server.route("/connecttest.txt", methods=["GET"])
-# # def hotspot(request):
-# #     print(request)
-# #     print("connecttest.txt")
-# #     return "", 200
-
-
-# @server.route("/redirect", methods=["GET"])
-# def hotspot(request):
-#     print(request)
-#     print("****************ms redir*********************")
-#     return redirect(f"http://{AP_DOMAIN}/", 302)
-
-# # # android redirects
-# # @server.route("/generate_204", methods=["GET"])
-# # def hotspot(request):
-# #     print(request)
-# #     print("******generate_204********")
-# #     return redirect(f"http://{AP_DOMAIN}/", 302)
-
-# # # apple redir
-# # @server.route("/hotspot-detect.html", methods=["GET"])
-# # def hotspot(request):
-# #     print(request)
-# #     """ Redirect to the Index Page """
-# #     return renderPage("index.html")
-
-
-# # @server.route("/wrong-host-redirect", methods=["GET"])
-# # def wrong_host_redirect(request):
-# #     print("******wrong host redirect********")
-# #     return "<!DOCTYPE html><head><meta http-equiv=\"refresh\" content=\"0;URL='http://" + AP_DOMAIN + "'/ /></head>"
-#     # return redirect(f"http://{AP_DOMAIN}/", 302)
-
-# # @server.route("/favicon.ico", methods=["GET"])
-# # def unnecessary(request):
-# #     return 'Not found', 404
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @server.catchall()
-# def catch_all(request):
-#     if request.headers.get("host").lower() != AP_DOMAIN.lower():
-#         # return redirect("http://" + AP_DOMAIN + "/redirect")
-#         return renderPage(f"{PAGES_PATH}/redirect.html", domain = AP_DOMAIN)
-#     return 'Not found', 404
-
-# # Serves the CSS file
-# def styles(request):
-#     return renderOther(f"{PAGES_PATH}/styles.css")
-
-# # Serves the JS file
-# def script(request):
-#     return renderOther(f"{PAGES_PATH}/script.js")
-
-# # Serves the JSON file
-# def config(request):
-#     return renderOther(f"./config.json")
-
-# def other(request):
-#     return renderOther(f"{PAGES_PATH}/random.html")
-
-# # This is where we start, which almost immediately takes us to the Login page
-# # def index(request):
-# #     print('in index')
-# #     print(request.headers.get("host"))
-# #     if request.headers.get("host").lower() != AP_DOMAIN.lower():
-# #         return renderPage(f"{PAGES_PATH}/redirect.html", domain = AP_DOMAIN.lower())
-
-# #     return renderPage(f"{PAGES_PATH}/login.html")
-# @server.route('/', methods = ['GET'])
-# def index(request):
-#     if request.headers.get("host").lower() != AP_DOMAIN.lower():
-#         return renderPage(f"{PAGES_PATH}/redirect.html", domain = AP_DOMAIN.lower())
-#     return renderPage(f"{PAGES_PATH}/login.html")
-
-# # Called when the user hits 'Login' on the Login page
-# @server.route('/login', methods = ['POST'])
-# def login(request):
-#     # with open(WIFI_FILE, "w") as f:
-#     #     json.dump(request.form, f)
-#     #     f.close()
-#     username = request.form['username']
-#     password = request.form['password']
-
-#     print('in login')
-
-#     # return renderPage(f"{PAGES_PATH}/main.html")
-#     return renderPage(f"{PAGES_PATH}/main.html",
-#                       time = getValueFromConfig('time'),
-#                       off_color = getValueFromConfig('off_color'),
-#                       on_color = getValueFromConfig('on_color'),
-#                       colon_color = getValueFromConfig('colon_color')
-#                       )
-
-# # Called when the user hits 'Submit' on the 'Change Colors' tab
-# def changeColors(request):
-#     off_color = request.form['offColorPicker']
-#     on_color = request.form['onColorPicker']
-#     colon_color = request.form['colonColorPicker']
-
-#     # Drop back to the Login page
-#     return renderPage(f"{PAGES_PATH}/login.html")
-
-# # Called when the user hits 'Submit' on the 'Set Time' tab
-# def changeTime(request):
-#     time = request.form['time']
-
-#     # Drop back to the Login page
-#     return renderPage(f"{PAGES_PATH}/login.html")
-
-# # Called when the user hits 'Change' on the 'Configure Login' tab
-# def changeLogin(request):
-#     new_username = request.form['newUsername']
-#     new_password = request.form['newPassword']
-
-#     # Drop back to the Login page
-#     return renderPage(f"{PAGES_PATH}/login.html")
-
-
-
-# # server.add_route("/", handler = index, methods = ["GET"])
-# # server.add_route("/login", handler = login, methods = ["POST"])
-# server.add_route("/change-colors", handler = changeColors, methods = ["POST"])
-# server.add_route("/change-time", handler = changeTime, methods = ["POST"])
-# server.add_route("/change-login", handler = changeLogin, methods = ["POST"])
-# server.add_route("/styles.css", handler = styles, methods = ["GET"]) # This captures requests for /styles.css and handles them
-# server.add_route("/script.js", handler = script, methods = ["GET"])
-# server.add_route("/config.json", handler = config, methods = ["GET"])
-# server.add_route("/favicon.ico", handler = unnecessary, methods = ["GET"])
-# server.add_route("/generate204", handler = catch_all, methods = ["GET"])
-# server.add_route("/generate_204", handler = catch_all, methods = ["GET"])
-# server.set_callback(catch_all)
-
-# ap = access_point(AP_NAME)
-# ip = ap.ifconfig()[0]
-# dns.run_catchall(ip)
-
-
-
-
-
-def getValueFromConfig(value):
-    with open(CONFIG_FILE_PATH, "r") as f:
+# Updates the specified keys in the config file with their new values
+def updateValuesInConfig(**kwargs):
+    with open(CONFIG_FILE_PATH, "r+") as f:
+        # Read file contents into variable for editing
         data = json.load(f)
-    return data[value]
+        # Augment the keys with new values
+        data.update(**kwargs)
+        # Delete the contents of the file
+        f.truncate(0)
+        # Make sure our cursor is at the beginning of the file
+        f.seek(0)
+        # Write the new contents to the file
+        json.dump(data, f, indent=4)
+    
 
 
+# Return the file as a string so it can be streamed to the connecting socket
+# Takes kwargs and can format the file with them
 def render(other, **kwargs):
     file_requested = ''
     # Read the passed-in file to a variable
@@ -208,35 +49,32 @@ def render(other, **kwargs):
         file_requested = file.read()
     # If kwargs have been passed in, augment the file to include them
     if kwargs:
-        file_requested.format(**kwargs)
+        file_requested = file_requested.format(**kwargs)
     return file_requested
 
 
-def new_styles():
-    print('in new_styles')
+def styles():
     return render(f'{PAGES_PATH}/styles.css')
 
-def new_javascript():
-    print('in new_script')
-    # Just pass the variables directly into here using your fancy, custom renderer
+def javascript():
+    # The reason I don't pass the variables from main() directly into the JS file here is that format doesn't work the same because of all the other {} in it
+    # TODO: Need to add "Content-Type: text/javascript" to a header somewhere
     return render(f'{PAGES_PATH}/script.js')
 
-def new_main():
-    print('in new_main')
-    return render(f'{PAGES_PATH}/main.html',
-                  	time = getValueFromConfig('time'),
-                    off_color = getValueFromConfig('off_color'),
-                    on_color = getValueFromConfig('on_color'),
-                    colon_color = getValueFromConfig('colon_color'))
+def login():
+    return render(f'{PAGES_PATH}/login.html')
 
+def main():
+    return render(f'{PAGES_PATH}/main.html', **getValuesFromConfig('time', 'off_color', 'on_color', 'colon_color'))
 
 
 
 # Available routes the AP will respond to
 routes = {
-    '/': new_main(),
-    '/styles.css': new_styles(),
-    '/script.js': new_javascript()
+    '/': login(),
+    '/main': main(),
+    '/styles.css': styles(),
+    '/script.js': javascript()
 }
 
 # Setup a custom AP
@@ -252,58 +90,38 @@ print('IP address to connect to: ' + ap.ifconfig()[0])
 
 # Once active, listen for connections
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# TODO: Put a try/catch around this
 s.bind(('', 80))
-s.listen(5)
+s.listen(3)
 
 # Connection loop (wait around, accept connections, respond to appropriate routes with the right content, etc)
 while True:
     conn, addr = s.accept()
     print('Got a connection from: %s' % str(addr))
     request = conn.recv(1024)
+    print('request', request)
 
-    print('Content = %s' % str(request))
-    if str(request) == b'':
-        print('weird shit')
-        continue
+    # print('Content = %s' % str(request))
+
     # Decode our request string and only get the route
-    # Looks like: "GET / HTTP/1.1 ......"
+    # Looks like: "GET /index HTTP/1.1 ......"
     route = request.decode().split()[1]
-    print('route', route)
+    if route == '/main':
+        # Request has the values at the end like this, so we isolate them off with split() magic
+        #       pt-Language: en-US,en;q=0.9\r\n\r\nusername=myusername&password=mypassword'
+        username, password = request.decode().split('\r\n')[-1].split('&')
+        # Save these values to the config file so they can be read later by the main.html handler
+        updateValuesInConfig(username=username.split('=')[-1], password=password.split('=')[-1])
 
     # Only respond to routes that are specified
-    if route == '' or route is None or route not in routes.keys():
+    if route == '' or route == b'' or route is None or route not in routes.keys():
         continue
 
     # Send the connected thing the appropriate page for the route they requested
-    conn.send(routes[route])
+    conn.sendall(routes[route])
 
-    # TODO: Should we close the connection?
-    # conn.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Counter-intuitively, this has to be here to make things work
+    conn.close()
 
 
 # Start the web server...
--- PicoWCode/pages/script.js
+++ PicoWCode/pages/script.js
@@ -1,14 +1,13 @@
-// Color of 0s when shown on the clock
+/* Color of 0s when shown on the clock */
 let off_color = '#000000';
-// Color of 1s when shown on the clock
-let on_color = '#FF0000';
-// Color of the colons in the middle of the clock (will fade between black and this color and back)
-let colon_color = '#FF0000';
-
+/* Color of 1s when shown on the clock */
+let on_color = '#000000';
+/* Color of the colons in the middle of the clock (will fade between black and this color and back) */
+let colon_color = '#000000';
 let time = '04:16';
 
 
-// Array of options for preset colors dropdown
+/* Array of options for preset colors dropdown */
 const presetColors = [
     { value: '#000000,#FFFFFF,#FFFFFF', text: 'Classy' },
     { value: '#000000,#FAE500,#FAE500', text: 'Radioactive' },
@@ -41,20 +40,20 @@ function initializeConfigVariables() {
     colon_color = document.getElementById("colon_color_variable").className;
 }
 
-// Set small hex colors
+/* Set small hex colors */
 function setColonColor(new_colon_color) {
-    document.getElementById('SH1').style.backgroundColor = new_colon_color
-    document.getElementById('SH2').style.backgroundColor = new_colon_color
+    document.getElementById('SH1').style.backgroundColor = new_colon_color;
+    document.getElementById('SH2').style.backgroundColor = new_colon_color;
 }
 
-// Set color picker initial values
+/* Set color picker initial values */
 function setPickerColors(new_off_color, new_on_color, new_colon_color) {
-    document.getElementById('offColorPicker').value = new_off_color
-    document.getElementById('onColorPicker').value = new_on_color
-    document.getElementById('colonColorPicker').value = new_colon_color
+    document.getElementById('offColorPicker').value = new_off_color;
+    document.getElementById('onColorPicker').value = new_on_color;
+    document.getElementById('colonColorPicker').value = new_colon_color;
 }
 
-// Set global color variables
+/* Set global color variables */
 function setColorVariables(new_off_color, new_on_color, new_colon_color) {
     off_color = new_off_color;
     on_color = new_on_color;
@@ -63,32 +62,32 @@ function setColorVariables(new_off_color, new_on_color, new_colon_color) {
 
 
 function initializeColors(new_off_color, new_on_color, new_colon_color) {
-    // Set color picker initial values
-    setPickerColors(new_off_color, new_on_color, new_colon_color)
+    /* Set color picker initial values */
+    setPickerColors(new_off_color, new_on_color, new_colon_color);
 
-    // Set off colors
-    document.getElementById('H1T2').style.borderColor = `${new_off_color} transparent`
-    document.getElementById('H1T4').style.borderColor = `${new_off_color} transparent`
-    document.getElementById('H1T6').style.borderColor = `${new_off_color} transparent`
-    document.getElementById('H2T1').style.borderColor = `${new_off_color} transparent`
-    document.getElementById('H2T3').style.borderColor = `${new_off_color} transparent`
-    document.getElementById('H2T5').style.borderColor = `${new_off_color} transparent`
+    /* Set off colors */
+    document.getElementById('H1T2').style.borderColor = `${new_off_color} transparent`;
+    document.getElementById('H1T4').style.borderColor = `${new_off_color} transparent`;
+    document.getElementById('H1T6').style.borderColor = `${new_off_color} transparent`;
+    document.getElementById('H2T1').style.borderColor = `${new_off_color} transparent`;
+    document.getElementById('H2T3').style.borderColor = `${new_off_color} transparent`;
+    document.getElementById('H2T5').style.borderColor = `${new_off_color} transparent`;
 
-    // Set on colors
-    document.getElementById('H1T1').style.borderColor = `${new_on_color} transparent`
-    document.getElementById('H1T3').style.borderColor = `${new_on_color} transparent`
-    document.getElementById('H1T5').style.borderColor = `${new_on_color} transparent`
-    document.getElementById('H2T2').style.borderColor = `${new_on_color} transparent`
-    document.getElementById('H2T4').style.borderColor = `${new_on_color} transparent`
-    document.getElementById('H2T6').style.borderColor = `${new_on_color} transparent`
+    /* Set on colors */
+    document.getElementById('H1T1').style.borderColor = `${new_on_color} transparent`;
+    document.getElementById('H1T3').style.borderColor = `${new_on_color} transparent`;
+    document.getElementById('H1T5').style.borderColor = `${new_on_color} transparent`;
+    document.getElementById('H2T2').style.borderColor = `${new_on_color} transparent`;
+    document.getElementById('H2T4').style.borderColor = `${new_on_color} transparent`;
+    document.getElementById('H2T6').style.borderColor = `${new_on_color} transparent`;
 
-    // Set small hex colors
-    setColonColor(new_colon_color)
+    /* Set small hex colors */
+    setColonColor(new_colon_color);
 }
 
 
 
-// Function to add options to select
+/* Function to add options to select */
 function populateSelect(selectId) {
     const selectElement = document.getElementById(selectId);
     presetColors.forEach(option => {
@@ -98,14 +97,14 @@ function populateSelect(selectId) {
 
 
 function getCurrentTimeInBinary() {
-    const time_pieces = time.split(':')
+    const time_pieces = time.split(':');
 
-    return [Number(time_pieces[0]).toString(2).padStart(6, '0'), Number(time_pieces[1]).toString(2).padStart(6, '0')]
+    return [Number(time_pieces[0]).toString(2).padStart(6, '0'), Number(time_pieces[1]).toString(2).padStart(6, '0')];
 }
 
 
 function changeCurrentTimeColors(color, oneOrZero) {
-    const time_pieces = getCurrentTimeInBinary()
+    const time_pieces = getCurrentTimeInBinary();
     const hours_triangles = [
         document.getElementById('H1T6'),
         document.getElementById('H1T5'),
@@ -113,7 +112,7 @@ function changeCurrentTimeColors(color, oneOrZero) {
         document.getElementById('H1T3'),
         document.getElementById('H1T2'),
         document.getElementById('H1T1')
-    ]
+    ];
 
     const minutes_triangles = [
         document.getElementById('H2T6'),
@@ -122,7 +121,7 @@ function changeCurrentTimeColors(color, oneOrZero) {
         document.getElementById('H2T3'),
         document.getElementById('H2T2'),
         document.getElementById('H2T1')
-    ]
+    ];
 
     Array.from(time_pieces[0]).forEach((bit, index) => {
         if (bit === oneOrZero) {
@@ -132,17 +131,17 @@ function changeCurrentTimeColors(color, oneOrZero) {
     
     Array.from(time_pieces[1]).forEach((bit, index) => {
         if (bit === oneOrZero) {
-            minutes_triangles[index].style.borderColor = `${color} transparent`
+            minutes_triangles[index].style.borderColor = `${color} transparent`;
         }
     });
 }
 
 
 function showCurrentTime(new_off_color, new_on_color, new_colon_color) {
-    changeCurrentTimeColors(new_off_color, '0')
-    changeCurrentTimeColors(new_on_color, '1')
-    setColonColor(new_colon_color)
-    setPickerColors(new_off_color, new_on_color, new_colon_color)
+    changeCurrentTimeColors(new_off_color, '0');
+    changeCurrentTimeColors(new_on_color, '1');
+    setColonColor(new_colon_color);
+    setPickerColors(new_off_color, new_on_color, new_colon_color);
 }
 
 
@@ -159,49 +158,49 @@ function getColorsFromPreset(preset_value) {
 
 
 
-// On page load
+/* On page load */
 document.addEventListener('DOMContentLoaded', function () {
-    // Open the first tab by default
+    /* Open the first tab by default */
     document.getElementsByClassName("tablinks")[0].click();
 
-    // Grab variables from HTML and set them here
-    initializeConfigVariables()
+    /* Grab variables from HTML and set them here */
+    initializeConfigVariables();
 
-    // Set the color of everything to the first preset
-    const preset_colors = getColorsFromPreset(presetColors[0].value)
-    setColorVariables(preset_colors['off_color'], preset_colors['on_color'], preset_colors['colon_color'])
-    initializeColors(preset_colors['off_color'], preset_colors['on_color'], preset_colors['colon_color'])
+    /* Set the color of everything to the first preset */
+    const preset_colors = getColorsFromPreset(presetColors[0].value);
+    setColorVariables(preset_colors['off_color'], preset_colors['on_color'], preset_colors['colon_color']);
+    initializeColors(preset_colors['off_color'], preset_colors['on_color'], preset_colors['colon_color']);
 
-    // Populate the preset colors dropdown with options
-    populateSelect('presetColorsDropdown')
+    /* Populate the preset colors dropdown with options */
+    populateSelect('presetColorsDropdown');
 
     
-    // Watch for changes to the showCurrentTime slider
+    /* Watch for changes to the showCurrentTime slider */
     const showCurrentTimeSlider = document.getElementById('showCurrentTimeSlider');
     showCurrentTimeSlider.addEventListener('input', function () {
-        show_current_time = !show_current_time
+        show_current_time = !show_current_time;
         if (show_current_time) {
-            showCurrentTime(off_color, on_color, colon_color)
+            showCurrentTime(off_color, on_color, colon_color);
         } else {
-            showPreviewTime()
+            showPreviewTime();
         }
-    })
+    });
 
 
-    // Watch for changes to the presetColorsDropdown
+    /* Watch for changes to the presetColorsDropdown */
     const presetColorsDropdown = document.getElementById('presetColorsDropdown');
     presetColorsDropdown.addEventListener('input', function () {
-        // Get colors from the preset value
-        const colors = getColorsFromPreset(presetColorsDropdown.value)
-        // Set global color variables to the new colors
-        setColorVariables(colors['off_color'], colors['on_color'], colors['colon_color'])
+        /* Get colors from the preset value */
+        const colors = getColorsFromPreset(presetColorsDropdown.value);
+        /* Set global color variables to the new colors */
+        setColorVariables(colors['off_color'], colors['on_color'], colors['colon_color']);
 
         if (show_current_time) {
-            showCurrentTime(colors['off_color'], colors['on_color'], colors['colon_color'])
+            showCurrentTime(colors['off_color'], colors['on_color'], colors['colon_color']);
         } else {
-            initializeColors(colors['off_color'], colors['on_color'], colors['colon_color'])
+            initializeColors(colors['off_color'], colors['on_color'], colors['colon_color']);
         }
-    })
+    });
 
 
 
@@ -209,49 +208,49 @@ document.addEventListener('DOMContentLoaded', function () {
     const onColorPicker = document.getElementById('onColorPicker');
     const colonColorPicker = document.getElementById('colonColorPicker');
 
-    // Watch for changes to the offColorPicker
+    /* Watch for changes to the offColorPicker */
     offColorPicker.addEventListener('input', function () {
         const color = offColorPicker.value;
         off_color = color;
 
         if (show_current_time) {
-            changeCurrentTimeColors(color, '0')
+            changeCurrentTimeColors(color, '0');
         } else {
-            showPreviewTime()
-            // document.getElementById('H1T2').style.borderColor = `${color} transparent`
-            // document.getElementById('H1T4').style.borderColor = `${color} transparent`
-            // document.getElementById('H1T6').style.borderColor = `${color} transparent`
-            // document.getElementById('H2T1').style.borderColor = `${color} transparent`
-            // document.getElementById('H2T3').style.borderColor = `${color} transparent`
-            // document.getElementById('H2T5').style.borderColor = `${color} transparent`
+            showPreviewTime();
+            /* document.getElementById('H1T2').style.borderColor = `${color} transparent` */
+            /* document.getElementById('H1T4').style.borderColor = `${color} transparent` */
+            /* document.getElementById('H1T6').style.borderColor = `${color} transparent` */
+            /* document.getElementById('H2T1').style.borderColor = `${color} transparent` */
+            /* document.getElementById('H2T3').style.borderColor = `${color} transparent` */
+            /* document.getElementById('H2T5').style.borderColor = `${color} transparent` */
         }
 
     });
 
-    // Watch for changes to the onColorPicker
+    /* Watch for changes to the onColorPicker */
     onColorPicker.addEventListener('input', function () {
         const color = onColorPicker.value;
         on_color = color;
 
         if (show_current_time) {
-            changeCurrentTimeColors(color, '1')
+            changeCurrentTimeColors(color, '1');
         } else {
-            showPreviewTime()
-            // document.getElementById('H1T1').style.borderColor = `${color} transparent`
-            // document.getElementById('H1T3').style.borderColor = `${color} transparent`
-            // document.getElementById('H1T5').style.borderColor = `${color} transparent`
-            // document.getElementById('H2T2').style.borderColor = `${color} transparent`
-            // document.getElementById('H2T4').style.borderColor = `${color} transparent`
-            // document.getElementById('H2T6').style.borderColor = `${color} transparent`
+            showPreviewTime();
+            /* document.getElementById('H1T1').style.borderColor = `${color} transparent` */
+            /* document.getElementById('H1T3').style.borderColor = `${color} transparent` */
+            /* document.getElementById('H1T5').style.borderColor = `${color} transparent` */
+            /* document.getElementById('H2T2').style.borderColor = `${color} transparent` */
+            /* document.getElementById('H2T4').style.borderColor = `${color} transparent` */
+            /* document.getElementById('H2T6').style.borderColor = `${color} transparent` */
         }
 
     });
 
-    // Watch for changes to the colonColorPicker
+    /* Watch for changes to the colonColorPicker */
     colonColorPicker.addEventListener('input', function () {
         const color = colonColorPicker.value;
         colon_color = color;
 
-        setColonColor(color)
+        setColonColor(color);
     });
 });
